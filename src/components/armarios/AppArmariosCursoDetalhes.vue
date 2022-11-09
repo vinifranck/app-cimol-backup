f2b0d2ad4810e42a261ad42df4975bbf3875d5a6
gabrielly <vargas.gabrielly16@gmail.com>
2022-11-09 16:29:34 -0300

    Atualização armarios

---
+++ src/components/armarios/AppArmariosCursoDetalhes.vue
@@ -0,0 +1,30 @@
+<template>
+    
+    <v-container>
+        <h1>Detalhes dos armários</h1>
+        <v-container fluid>
+            <v-row dense>
+                <v-col>
+                    <v-card>
+                        <v-card-title>
+                        </v-card-title>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+        <back-button/>
+    </v-container>
+</template>
+
+<script>
+    import api from '../api/api';
+    import Loading from '../loading/Loading.vue';
+    import BackButton from '../navigation/BackButton.vue';
+    export default {
+        name: 'AppCursoDetalhes',
+        components: { Loading, BackButton},
+        mixins: [api],
+        
+    }
+    let params=new URL(location.href).searchParams.get('id_curso');
+</script>
