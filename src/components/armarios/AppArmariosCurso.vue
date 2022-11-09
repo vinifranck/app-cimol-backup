50c526dc18602defd3e47396c14d86a42cf86074
gabrielly <vargas.gabrielly16@gmail.com>
2022-11-09 15:32:39 -0300

    Atualização lista de armários

--- src/components/armarios/AppArmariosCurso.vue
+++ src/components/armarios/AppArmariosCurso.vue
@@ -7,13 +7,13 @@
                 <v-col
                 v-for="armario in armarios"
                 :key="armario.id_armario"
-                :cols="armario.numero"
+                
                 >
-                    <v-card>
+                    <v-card color="green">
                         <v-card-title v-text="armario.numero"></v-card-title>
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            <v-btn text @click="detalhesArmario(armario.id_curso, armario.id_armario)">
+                            <v-btn text @click="detalhesArmario(armario.id_armario)">
                                 Ver mais ...
                             </v-btn>
                         </v-card-actions>
@@ -50,10 +50,22 @@
             });
         },
         methods:{
-            detalhesArmario(id_curso, id_armario){
-               // this.$router.push("/curso/armario/"+id_curso+"/"+id_armario);
+            detalhesArmario(id_armario){
+               this.$router.push("/curso/armario/"+this.$route.params.id_curso+"/"+id_armario);
             }
         }
     }
     let params=new URL(location.href).searchParams.get('id_curso');
 </script>
+
+<style>
+    .v-col{
+        display:grid;
+        row-gap: 10px;
+        grid-template-columns:auto auto auto auto auto auto;
+    }
+
+   
+
+
+</style>
